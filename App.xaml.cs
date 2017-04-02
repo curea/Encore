74e2099ccf4b264b38d3e018d70017d322104eba
Aaron Cure <aaron.cure@cypressdefense.com>
2017-04-02 16:27:57 -0600

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Avalonia;
+using Avalonia.Controls;
+//using Avalonia.Logging.Serilog;
+using Avalonia.Markup.Xaml;
+
+namespace Encore
+{
+	public class App : Application
+	{
+		public override void Initialize()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+
+		private static void Main(string[] args)
+		{
+			AppBuilder.Configure<App>()
+				.UsePlatformDetect()
+				.Start<MainWindow>();
+		}
+	}
+}
